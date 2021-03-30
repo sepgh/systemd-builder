2f15fb62354f50afab771e7ca5b133f211ebec26
sepehr-gh <sepehrgh01@gmail.com>
2021-03-30 17:07:25 +0430

    feat: sudo aware

--- sysd-builder.sh
+++ sysd-builder.sh
@@ -5,8 +5,8 @@ echo "Enter service name (english alphabet and numbers, no spaces or anything): 
 read service_name 
 if [ -z "$service_name" ]
 then
-        echo "service name is required."
-        exit
+    echo "service name is required."
+    exit
 fi 
 
 
@@ -15,7 +15,7 @@ read description
 
 if [ ! -z "$description" ]
 then
-        description=":$description"
+    description=": $description"
 fi    
 
 
@@ -37,7 +37,7 @@ echo "Enter arguments: "
 read arguments
 if [ ! -z "$arguments" ]
 then
-        arguments=" $arguments"
+    arguments="$arguments"
 fi    
 
 echo "Enter user (default: root): "
@@ -45,7 +45,7 @@ read user
 
 if [ -z "$user" ]
 then
-        user='root'
+    user='root'
 fi 
 
 # make file
@@ -56,8 +56,17 @@ sed -i 's~$command~'$command'~g' $service_name.service
 sed -i 's~$arguments~'$arguments'~g' $service_name.service
 sed -i 's/$user/'$user'/g' $service_name.service
 
-mv $service_name.service /etc/systemd/system
-systemctl enable $service_name
+echo "Enter any key and press enter to install in sudo mode. (are you not a sudoer/root?)"
+read is_sudo
+if [ -z "$is_sudo" ]
+then
+    sudo mv $service_name.service /etc/systemd/system
+    sudo systemctl enable $service_name
+else
+    mv $service_name.service /etc/systemd/system
+    systemctl enable $service_name
+fi
+
 
 echo "Done! Service is now enabled. You can control it with 'service $service_name start|stop|reload'"
 
