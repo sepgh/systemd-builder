e12cf42d78599b8895fa6eff5fe8daecc8f715c1
sepehr-gh <sepehrgh01@gmail.com>
2021-03-30 16:51:24 +0430

    init

---
+++ sysd-builder.sh
@@ -0,0 +1,57 @@
+#!/bin/sh
+
+
+echo "Enter service name (english alphabet and numbers, no spaces or anything): "
+read service_name 
+if [ -z "$service_name" ]
+then
+        echo "service name is required."
+        exit
+fi 
+
+
+echo "Enter service description: "
+read description 
+
+if [ ! -z "$description" ]
+then
+        description=":$description"
+fi    
+
+
+echo "Enter command: "
+read command
+valid="-1"
+while [ "$valid" = '-1' ]
+do
+    if [ -z "$command" ];
+    then
+        echo "Invalid command. Can't be empty. Enter command again: ";
+        read command
+    else
+        valid="1"
+    fi
+done
+
+echo "Enter arguments: "
+read arguments
+if [ ! -z "$arguments" ]
+then
+        arguments=" $arguments"
+fi    
+
+echo "Enter user (default: root): "
+read user
+
+if [ -z "$user" ]
+then
+        user='root'
+fi 
+
+# make file
+cp sample.service $service_name.service
+sed -i 's/$service_name/'$service_name'/g' $service_name.service
+sed -i 's/$description/'$description'/g' $service_name.service
+sed -i 's~$command~'$command'~g' $service_name.service
+sed -i 's~$arguments~'$arguments'~g' $service_name.service
+sed -i 's/$user/'$user'/g' $service_name.service
