4333966d62953690a560b5614428e040ab570721
sepehr-gh <sepehrgh01@gmail.com>
2021-03-31 18:51:29 +0430

    fix: issue with missing sample.service and special characters

--- sysd-builder.sh
+++ sysd-builder.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
+# Validating existance of sample.service
+if [ ! -f sample.service ] ; then
+    echo "You need 'sample.service' in order to run this script"
+    if command -v curl &> /dev/null || command -v wget &> /dev/null
+    then
+        wget https://raw.githubusercontent.com/sepehr-gh/systemd-builder/main/sample.service 2>/dev/null || curl -O  https://raw.githubusercontent.com/sepehr-gh/systemd-builder/main/sample.service
+    else
+        exit
+    fi
+fi
+
 
 echo "Enter service name (english alphabet and numbers, no spaces or anything): "
 read service_name 
@@ -11,12 +22,7 @@ fi
 
 
 echo "Enter service description: "
-read description 
-
-if [ ! -z "$description" ]
-then
-    description=": $description"
-fi    
+read description
 
 
 echo "Enter command: "
@@ -48,13 +54,16 @@ then
     user='root'
 fi 
 
-# make file
-cp sample.service $service_name.service
-sed -i 's/$service_name/'$service_name'/g' $service_name.service
-sed -i 's/$description/'$description'/g' $service_name.service
-sed -i 's~$command~'$command'~g' $service_name.service
-sed -i 's~$arguments~'$arguments'~g' $service_name.service
-sed -i 's/$user/'$user'/g' $service_name.service
+
+
+# Make file
+rm $service_name.service 2> /dev/null || echo > /dev/null
+cp sample.service $service_name.service 
+
+sed -i -e "s/var_description/$description/g" $service_name.service
+sed -i -e "s~var_command~$command~g" $service_name.service
+sed -i -e "s~var_arguments~$arguments~g" $service_name.service
+sed -i -e "s/var_user/$user/g" $service_name.service
 
 echo "Enter any key and press enter to install in sudo mode. (are you not a sudoer/root?)"
 read is_sudo
